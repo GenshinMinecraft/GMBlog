c84eb3a4bc21e12769ff7cbf19623791760525d9
GenshinMinecraft <genshinminecraft114514@gmail.com>
2024-02-20 12:12:17 +0800

    Tue Feb 20 12:12:17 CST 2024

--- buildpush.sh
+++ buildpush.sh
@@ -1,9 +1,11 @@
 #/bin/bash
 
 sudo chmod 777 * -R
-#rm -rf ./resources ./public
+rm -rf ./resources ./public
 hugo
 #rm -rf ./resources
 git add .
 git commit -m "`date`"
 git push
+cd public/
+bash zip.sh
